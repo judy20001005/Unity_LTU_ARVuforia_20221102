27cf89e4812f247abd9dccd4d22972fb3de304be
judy20001005 <94591774+judy20001005@users.noreply.github.com>
2022-12-07 18:49:02 +0800

    追蹤系統

--- Assets/Scripts/TrackSystem.cs
+++ Assets/Scripts/TrackSystem.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TrackSystem : MonoBehaviour
 {
@@ -11,13 +10,23 @@ public class TrackSystem : MonoBehaviour
 
     private string nameTarget = "大吉";
     private Transform pointTarget;
-    private UnityEngine.AI.NavMeshAgent nma;
+    private NavMeshAgent nma;
+    private Animation ani;
+    private string parWalk = "開關走路";
+
 
 
     private void Awake()
     {
         //目標物件的座標資訊 = 遊戲物件,尋找(目標物名稱)轉型為變形
         pointTarget = GameObject.Find(nameTarget).transform;
+
+        //nma = 取得元件<導覽網格代理器>() - 將代理器元件存放至 nma
+        nma = GetComponent<NavMeshAgent>();
+        nma.speed = speed;
+        nma.stoppingDistance = stoppintDistance;
+
+        ani = GetComponent<Animation>();
     }
     // Start is called before the first frame update
     void Start()
@@ -26,8 +35,20 @@ public class TrackSystem : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    //更新事件:一秒鐘約60次執行，60FPS(每秒幀數)
+    private void Update()
     {
-        
+        TrackTarget();
+    }
+
+    private void TrackTarget()
+    {
+        //代理器.設定目的地(目標物件的座標)
+        nma.SetDestination(pointTarget.position);
+
+
+
+        print($"<color=yellow>代理器的加速度 : {nma.velocity.magnitude}</color>");
+        //ani.SetBool(parWalk, nma.velocity.magnitude != 0);
     }
 }
